dbf0b71999b5599119bfccce35ed807e8d1ad99e
Ali Emamzadeh <109407589+aliEm2001@users.noreply.github.com>
2022-07-17 15:09:05 +0430

    Add files via upload

---
+++ FileReader/src/com/FileReader/Main.java
@@ -0,0 +1,21 @@
+package com.FileReader;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    FileReader fileReader = new FileReader();
+
+        ArrayList<Doc> documents = fileReader.readFiles();
+
+
+    }
+}
+
+
+
+
