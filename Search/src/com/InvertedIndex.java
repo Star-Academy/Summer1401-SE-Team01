93f8b2c306efc74fc6cc96bd4172876bfca034d1
SeyyedMohammadHossein Hosseini <s.m.m.m.h2013@gmail.com>
2022-07-19 16:46:44 +0430

    merged?

--- Search/src/com/InvertedIndex.java
+++ Search/src/com/InvertedIndex.java
@@ -1,5 +1,6 @@
 package com;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -16,6 +17,28 @@ public class InvertedIndex {
     public static HashSet<Doc> get(String word) {
         HashSet<Doc> answer = invertedIndex.get(word);
         if (answer == null) return new HashSet<>();
+        return answer;
+    }
+
+    public static HashSet<Doc> get(ArrayList<String> includeAll, ArrayList<String> excludeAll, ArrayList<String> includeOne, ArrayList<Doc> documents) {
+        HashSet<Doc> answer = new HashSet<>();
+
+        if(includeOne.isEmpty())
+            answer = new HashSet<>(documents);
+        else {
+            for(String word : includeOne) {
+                answer.addAll(InvertedIndex.get(word.substring(1)));
+            }
+        }
+
+        for (String word : includeAll) {
+            answer.retainAll(InvertedIndex.get(word));
+        }
+
+        for (String word : excludeAll) {
+            answer.removeAll(InvertedIndex.get(word.substring(1)));
+        }
+
         return answer;
     }
 }
--- Search/src/com/Main.java
+++ Search/src/com/Main.java
@@ -12,11 +12,29 @@ public class Main {
         for (Doc document: documents) InvertedIndex.add(document);
 
         Scanner scanner = new Scanner(System.in);
-        String keyWord = scanner.next().toUpperCase();
-        HashSet<Doc> answer = InvertedIndex.get(keyWord);
+        String keyWord = scanner.nextLine().toUpperCase();
+        String [] words = keyWord.split(" +");
+
+        ArrayList<String> includeAll = new ArrayList<>();
+        ArrayList<String> excludeAll = new ArrayList<>();
+        ArrayList<String> includeOne = new ArrayList<>();
+
+        for(String word: words) {
+            if(word.startsWith("+"))
+                includeOne.add(word);
+            else if(word.startsWith("-"))
+                excludeAll.add(word);
+            else
+                includeAll.add(word);
+        }
+
+        HashSet<Doc> answer = InvertedIndex.get(includeAll, excludeAll, includeOne, documents);
+
         for (Doc document: answer) {
             System.out.println(document.getName());
         }
         scanner.close();
     }
 }
+
+
