13ba1609790e3f110896dbea88a7f7d1d432de6b
SeyyedMohammadHossein Hosseini <s.m.m.m.h2013@gmail.com>
2022-07-29 14:49:02 +0430

    add expected value to the inlineData

--- SimpleCalculator/SimpleCalculatorTest/Test.cs
+++ SimpleCalculator/SimpleCalculatorTest/Test.cs
@@ -8,12 +8,11 @@ namespace SimpleCalculatorTest;
 public class Test
 {
     [Theory]
-    [InlineData(2, 3)]
-    [InlineData(5, 6)]
-    [InlineData(13, 6)]
-    public void Calculate_Sum_SumOfTwoNumbers(int firstOperand, int secondOperand)
+    [InlineData(2, 3, 5)]
+    [InlineData(5, 6, 11)]
+    [InlineData(13, 6, 19)]
+    public void Calculate_Sum_SumOfTwoNumbers(int firstOperand, int secondOperand, int expected)
     {
-        int expected = firstOperand + secondOperand;
         Calculator calculator = new Calculator();
 
         int answer = calculator.Calculate(firstOperand, secondOperand, OperatorEnum.sum);
@@ -22,14 +21,13 @@ public class Test
     }
 
     [Theory]
-    [InlineData(0, 1000)]
-    [InlineData(-100, 1000)]
-    [InlineData(2, 3)]
-    [InlineData(5, 6)]
-    [InlineData(13, 6)]
-    public void Calculate_Multiply_MultiplyOfTwoNumbers(int firstOperand, int secondOperand)
+    [InlineData(0, 1000, 0)]
+    [InlineData(-100, 1000, -100000)]
+    [InlineData(2, 3, 6)]
+    [InlineData(5, 6, 30)]
+    [InlineData(13, 6, 13*6)]
+    public void Calculate_Multiply_MultiplyOfTwoNumbers(int firstOperand, int secondOperand, int expected)
     {
-        int expected = firstOperand * secondOperand;
         Calculator calculator = new Calculator();
 
         int answer = calculator.Calculate(firstOperand, secondOperand, OperatorEnum.multiply);
@@ -38,14 +36,13 @@ public class Test
     }
 
     [Theory]
-    [InlineData(0, 1000)]
-    [InlineData(-100, 1000)]
-    [InlineData(2, 3)]
-    [InlineData(5, 6)]
-    [InlineData(13, 6)]
-    public void Calculate_Division_DivisionOfTwoNumbers(int firstOperand, int secondOperand)
+    [InlineData(0, 1000, 0)]
+    [InlineData(-100, 1000, 0)]
+    [InlineData(2, 3, 0)]
+    [InlineData(5, 6, 0)]
+    [InlineData(13, 6, 2)]
+    public void Calculate_Division_DivisionOfTwoNumbers(int firstOperand, int secondOperand, int expected)
     {
-        int expected = firstOperand / secondOperand;
         Calculator calculator = new Calculator();
 
         int answer = calculator.Calculate(firstOperand, secondOperand, OperatorEnum.division);
