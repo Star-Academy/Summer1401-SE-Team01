40188041790ee2d6613c6a91d96e60e1c405adee
Ali Emamzadeh <ali.emamzadeh12@gmail.com>
2022-07-31 17:00:38 +0430

    add invertedIndex

--- SearchTDD/Search.Test/ISearchHandlerTest.cs
+++ SearchTDD/Search.Test/ISearchHandlerTest.cs
@@ -6,10 +6,29 @@ public class ISearchHandlerTest
     public ISearchHandlerTest()
     {
         InvertedIndex invertedIndex = new InvertedIndex();
+        invertedIndex.Database = new Dictionary<string, IEnumerable<string>>()
+        {
+            ["THIS"] = new[] { "1", "3" },
+            ["IS"] = new[] { "1", "2" },
+            ["A"] = new[] { "1", "2" },
+            ["TEXT"] = new[] { "1" },
+            ["DOCUMENT"] = new[] { "1" },
+            ["!"] = new[] { "1" },
+            ["HELLO"] = new[] { "2", "3" },
+            ["WHAT"] = new[] { "2" },
+            ["GREAT"] = new[] { "2" },
+            ["DAY"] = new []{"2"},
+            ["IT"] = new []{"2"},
+            ["PLEASE"] = new []{"3"},
+            ["PUT"] = new []{"3"},
+            ["INTO"] = new []{"3"},
+            ["THE"] = new []{"3"},
+            ["MICROWAVE"] = new []{"3"},
+
+        };
         
         // _searchHandler = new IncludeOneHandler();
     }
     
-    [Fact]
-    public void 
+    
 }
