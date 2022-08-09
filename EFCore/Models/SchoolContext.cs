decad05c0e59978731105cb6a54e495e69da7589
Ali Emamzadeh <ali.emamzadeh12@gmail.com>
2022-08-09 17:01:54 +0430

    first implement

---
+++ EFCore/Models/SchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Models;
+
+public class SchoolContext : DbContext
+{
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Grade> Grades { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(
+            "Server=localhost;Port=5432;Database=school;User Id=postgres;Password=password;");
+        
+    }
+}
