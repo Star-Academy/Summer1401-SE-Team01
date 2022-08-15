415e4b5b15320e462c3df3987b876fc4c0415db2
Ali Emamzadeh <ali.emamzadeh12@gmail.com>
2022-08-15 15:08:16 +0430

    init

---
+++ ASP.Net Core/SearchAPI/SearchAPI/Controllers/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Search;
+
+namespace SearchAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SearchController : Controller
+{
+    static readonly string _filePath = Path.GetFullPath(@"TestFiles");
+    private ISearchEngine _searchEngine;
+    
+    public SearchController()
+    {
+        var fileProvider = new FileProvider();
+        var documents = fileProvider.GetData(_filePath);
+        var invertedIndex = new InvertedIndexBuilder().Build(documents);
+        var includeAllHandler = new IncludeAllHandler();
+        includeAllHandler.Next = new IncludeOneHandler();
+        includeAllHandler.Next.Next = new ExcludeAllHandler();
+
+        var searchEngine = new SearchEngine(invertedIndex, includeAllHandler);
+    }
+
+    [HttpGet("[action]")]
+    public ActionResult<IEnumerable<string>> Query(string query)
+    {
+        return Ok(_searchEngine.Query(query));
+    }
+}
