8cb87cfe59a5a531ba601a8e65e1259495b44321
Ali Emamzadeh <109407589+aliEm2001@users.noreply.github.com>
2022-08-15 16:07:39 +0430

    Asp (#6)
    * init
    
    * debug
    
    * some refactoring
    
    * add test files
    
    * refactor

---
+++ ASP.Net Core/SearchAPI/SearchAPI/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using Search;
+
+namespace SearchAPI;
+
+public class Configuration
+{
+    public static void DependencyInjectionConfiguration(WebApplicationBuilder webApplicationBuilder)
+    {
+        string _filePath = Path.GetFullPath(@"TestFiles");
+        var fileProvider = new FileProvider();
+        var documents = fileProvider.GetData(_filePath);
+        var includeAllHandler = new IncludeAllHandler();
+        includeAllHandler.Next = new IncludeOneHandler();
+        includeAllHandler.Next.Next = new ExcludeAllHandler();
+
+        webApplicationBuilder.Services.AddSingleton<ISearchHandler>(includeAllHandler);
+        webApplicationBuilder.Services.AddSingleton<InvertedIndex>(new InvertedIndexBuilder().Build(documents));
+        webApplicationBuilder.Services.AddSingleton<ISearchEngine, SearchEngine>();
+    }
+}
---
+++ ASP.Net Core/SearchAPI/SearchAPI/Controllers/SearchController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Search;
+
+namespace SearchAPI.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class SearchController : Controller
+{
+    private ISearchEngine _searchEngine;
+    
+    public SearchController(ISearchEngine searchEngine)
+    {
+        _searchEngine = searchEngine;
+    }
+    
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Search(string query)
+    {
+        return Ok(_searchEngine.Query(query));
+    }
+}
